f1c6207f28b1a1e3b87cd7f285169272626e756b
MilchMische <akinorkan@gmail.com>
2024-12-06 09:05:13 +0100

    Update App.py

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ import time
 # Funktion zum Laden der hochgeladenen Datei und zur Inspektion des Dateiinhalts
 def load_data(uploaded_file):
     try:
-        # Datei einlesen und nur die Spalten mit den relevanten Daten behalten
+        # Datei einlesen und nur die Spalten mit den relevanten Daten behalten, fehlerhafte Zeilen überspringen
         data = pd.read_csv(uploaded_file, sep="\t", header=None, usecols=[1, 3], on_bad_lines="skip")
         
         # Überprüfen, ob mindestens 2 Spalten vorhanden sind
